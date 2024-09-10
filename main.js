aa3a8bb59ca7f1f8fbe1c597b64172678b6f9f6c
Kevin <NeroAzure@gmx.de>
2024-09-10 21:02:32 +0200

    ip time

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 var renderTime = function () {
-	var time = new Date();
-	clock.textContent = time.toLocaleString('de-DE', {year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: 'numeric', hour12: false});
+	const time = new Date();
+	const timeString =  time.toLocaleString(
+		'de-DE', 
+		{
+			year: 'numeric', 
+			month: '2-digit', 
+			day: '2-digit', 
+			hour: '2-digit', 
+			minute: '2-digit', 
+			second: 'numeric', 
+			hour12: false
+		},
+	);
+
+	let part1 = timeString.substring(0, 9);
+	let part2 = timeString.substring(9, 18);
+	let part3 = timeString.substring(18);
+
+	clock.textContent = `${part1}.${part2}:${part3}`;
 };
 setInterval(renderTime, 1000);
